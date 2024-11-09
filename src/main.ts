f565ed633c4e87965c69c3def08f7616340ee627
aaronburt <email@aaronburt.co.uk>
2024-11-09 00:46:29 +0000

    feat: :fire: Added nodemon.json, amended main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,39 +1,42 @@
 import express, { Express, Request, Response } from 'express';
-import multer from 'multer';
-import disk from './model/diskStorage.js';
-import authenticator from './controller/middleware.js';
+import { PrismaClient } from '@prisma/client';
 
+import multer from 'multer';
 import path from 'path';
 import fs from 'fs';
+
+import disk from './model/diskStorage.js';
+import authenticator from './controller/middleware.js';
 
 const upload: multer.Multer = multer({ storage: disk });
 const app: Express = express();
 
-import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 app.get('/v/:id', async(req: Request, res: Response) => {
+    try {
+        const record = await prisma.file.findFirst({ where: { id: req.params.id } })
 
-    const record = await prisma.file.findFirst({ where: { id: req.params.id } })
-
-    if(record){
-        const file = path.resolve(path.join(import.meta.dirname, '..', 'upload', record.id));
-        if(fs.existsSync(file)){
-            return res.sendFile(file);
+        if(record){
+            const file = path.resolve(path.join(import.meta.dirname, '..', 'upload', record.id));
+            if(fs.existsSync(file)){
+                return res.sendFile(file);
+            }
         }
+    
+        return res.sendStatus(400)
+    } catch(error: any){
+        console.warn(error);
+        return res.sendStatus(500);
     }
-
-    return res.sendStatus(400)
-
-})
-
+});
 
 app.post('/upload', authenticator, upload.single('file'), async(req: Request, res: Response) => {
     try {
         if(req.file){
 
             const { filename, originalname, mimetype, size } = req.file;
-            await prisma.file.create({
+            const file = await prisma.file.create({
                 data: {
                     id: filename,
                     originalname: originalname,
@@ -41,8 +44,8 @@ app.post('/upload', authenticator, upload.single('file'), async(req: Request, re
                     size: size
                 }
             });
-    
-            return res.sendStatus(200);
+
+            return res.json(file);
         }
     
         return res.sendStatus(400);
@@ -50,7 +53,6 @@ app.post('/upload', authenticator, upload.single('file'), async(req: Request, re
         console.warn(error);
         return res.sendStatus(500);
     }
-})
-
+});
 
 app.listen(3000);
